1ae352d629b8dbe8e99dcd5ed1302454275fab81
MD ALTAMASH <altamash3321@gmail.com>
2023-11-01 09:57:15 +0530

    Initial commit

---
+++ src/main/java/com/example/gaming_console/GamingConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.example.gaming_console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingConsoleApplication.class, args);
+	}
+
+}
